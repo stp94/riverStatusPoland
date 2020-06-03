27beeb6ae5526a44f584aeb03b6c5ac903f5c55e
stp94 <38986323+stp94@users.noreply.github.com>
2020-06-03 20:21:27 +0200

    work in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled1';
+  title = 'Twoja rzeka';
+  station = '';
+  state = '';
+  river = '';
+
+  arrRivers: string [];
+
+  constructor(private httpService: HttpClient) { }
+
+  inputStation(station: string) {
+    this.station = station;
+  }
+
+  inputState(state: any) {
+    this.state = state;
+  }
+
+  inputRiver(river: any) {
+    this.river = river;
+  }
+
+
+  searchRiver() {
+    console.log(this.station, this.river, this.state);
+
+    this.httpService.get('./assets/json/response.json').subscribe(
+      data => {
+        this.arrRivers = data as string [];
+        //console.log(this.arrRivers[1].stacja);
+      }
+    );
+    // for (let i = 0; i < this.arrRivers.length; i++){
+    //   if (this.station === this.arrRivers[i].stacja){
+    //     console.log(this.arrRivers[i]);
+    //   }
+    // }
+
+
+    for (let i = 0; i < this.arrRivers.length; i++){
+      if (this.state === this.arrRivers[i].województwo){
+        console.log(this.arrRivers[i]);
+      }
+    }
+
+
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
